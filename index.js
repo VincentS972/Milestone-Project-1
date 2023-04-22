d8aa8a575ace96ad66fe0e7e5a2e204b1ddbdeaf
VincentS972 <vincentsechi@gmail.com>
2023-04-22 11:39:13 -0400

    corrected files location

---
+++ index.js
@@ -0,0 +1,123 @@
+
+// const npc = oldLady(-150, 110)
+
+// const obstacle = crate (100, 110)
+
+// // have the NPC start walking east immediately
+// async function moveObstacle(){
+   
+//     await obstacle.walkWest(10000)
+    
+// }
+// moveObstacle()
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const kid = document.querySelector('#character')
+    let bottom = 0
+    let gravity = 0.9
+    let isJumping = false
+    const jumpid = document.getElementById('jumping');
+    jumpid.style.display = 'block';
+
+
+
+
+    function characterjump(){
+    if (isJumping) return
+    let timerUpId = setInterval(function() {
+
+        if (bottom > 250){
+            clearInterval(timerUpId)
+            let timerDownId = setInterval (function(){
+                if (bottom < 0){
+                    clearInterval(timerDownId)
+                    isJumping = false
+                }
+                bottom -=5
+                kid.style.bottom = bottom +'px'
+            },20)
+    }
+    isJumping = true    
+    bottom += 30
+    bottom = bottom * gravity
+    kid.style.bottom = bottom +'px'
+    }, 20)}
+
+
+
+    function control(e) {
+        if (e.keyCode === 38){
+        characterjump()
+    }
+ }
+
+
+
+document.addEventListener('keydown', control)})
+
+
+
+
+// function crate(x, y) {
+//     let element = newImage('assets/crate.png')
+//     element.style.zIndex = 1;
+//     element.style.height
+
+    
+//     let direction = null;
+
+//     function moveCharacter() {
+        
+//         if (direction === 'west') {
+//             x += 1
+//         }
+ 
+//         element.style.right = x + 'px'
+//         element.style.bottom = y + 'px'
+//     }
+
+//     setInterval(moveCharacter, 1)
+
+//     async function walkWest(time) {
+//         direction ='west'
+//         element.src = `./assets/crate.png`
+//         await sleep(time);
+//         stop();
+//     }
+
+//     function stop() {
+//         direction = null
+//         element.src = `./assets/crate.png`
+//     }
+
+//     return {
+//         element: element,   
+//         walkWest: walkWest,
+//         stop: stop
+//     }
+// }
+
+// function sleep(time){
+//     return new Promise(resolve => {
+//         setTimeout(resolve, time)
+//     })  
+// }
+function newImage(url){
+    let image = document.createElement('img')
+    image.src = url
+    image.style.position = 'absolute'
+    document.body.append(image)
+    return image
+}
+
+
+
+
+
+
+
+
+
